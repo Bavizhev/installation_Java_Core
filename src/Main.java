3e4bb46fc6d344fe01c939b69afb9ec2ca694487
Bavizhev <ilyas4bavizhev@gmail.com>
2023-08-09 21:54:48 +0300

    Bavizhev v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,32 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         StringBuilder textInfo = new StringBuilder();
 
-        File main = new File("D://Games", "src/main/");
-        File test = new File("D://Games", "src/test/");
-        File savegames = new File("D://Games", "savegames");
-        File temp = new File("D://Games", "temp");
+        ArrayList<String> pathsMk = new ArrayList<>();
 
-        createDirs(main, textInfo); // Создание сразу поддиректорий
-        createDirs(test, textInfo);
+        pathsMk.add("D://Games//src///main//");
+        pathsMk.add("D://Games//src///test//");
+        pathsMk.add("D://Games//savegames//");
+        pathsMk.add("D://Games//temp//");
+        pathsMk.add("D://Games//res//drawables//");
+        pathsMk.add("D://Games//res//vectors//");
+        pathsMk.add("D://Games//res//icons//");
 
-        createDir(savegames, textInfo); // Создание одной директорий
-        createDir(temp, textInfo);
+        createDirs(pathsMk, textInfo);
 
-        File drawables = new File("D://Games", "res/drawables/");
-        File vectors = new File("D://Games", "res/vectors/");
-        File icons = new File("D://Games", "res/icons/");
+        ArrayList<String> pathsFiles = new ArrayList<>();
 
-        createDirs(drawables, textInfo);
-        createDirs(vectors, textInfo);
-        createDirs(icons, textInfo);
+        pathsFiles.add("D://Games//src//main//Main.java");
+        pathsFiles.add("D://Games//src//main//Utils.java");
+        pathsFiles.add("D://Games//temp//temp.txt");
 
-        File mainFile = new File("D://Games//src//main//Main.java");
-        File utilsFile = new File("D://Games//src//main//Utils.java");
-        File tempFile = new File("D://Games//temp//temp.txt");
-
-        createFiles(mainFile, textInfo); //Создание файлов
-        createFiles(utilsFile, textInfo);
-        createFiles(tempFile, textInfo);
-
+        createFiles(pathsFiles, textInfo);
 
         try {
             FileWriter text = new FileWriter("D://Games//temp//temp.txt", false);
@@ -45,29 +38,27 @@ public class Main {
 
     }
 
-    public static void createDir(File dir, StringBuilder textInfo) { // <--- Для создания одной директорий
-        if (dir.mkdir()) {
-            textInfo.append("Directory " + dir.getName() + " (" + dir.getParent() + ")" + " created \n");
-        } else {
-            textInfo.append("Directory " + dir.getName() + " not created or already exists (ERROR)\n");
+    public static void createDirs(ArrayList<String> pathsMk, StringBuilder textInfo) {
+        for (String path : pathsMk) {
+            File file = new File(path);
+            if (file.mkdirs()) {
+                textInfo.append("Directory " + file.getName() + " (" + file.getParent() + ")" + " created \n");
+            } else {
+                textInfo.append("Directory " + file.getName() + " not created or already exists (ERROR)\n");
+            }
         }
     }
 
-    public static void createDirs(File dirs, StringBuilder textInfo) { // <--- Для создания иерархий директорий (директория/поддиректория/...)
-        if (dirs.mkdirs()) {
-            textInfo.append("Directory " + dirs.getName() + " (" + dirs.getParent() + ")" + " created \n");
-        } else {
-            textInfo.append("Directory " + dirs.getName() + " not created or already exists (ERROR)\n");
-        }
-    }
-
-    public static void createFiles(File file, StringBuilder textInfo) {
-        try {
-            file.createNewFile();
-            textInfo.append("File " + file.getName() + " created \n");
-        } catch (IOException e) {
-            textInfo.append("File " + file.getName() + " not  created (" + e.getMessage() + ") (ERROR) \n");
-            System.out.println(e.getMessage());
+    public static void createFiles(ArrayList<String> pathsFiles, StringBuilder textInfo) {
+        for (String path : pathsFiles) {
+            File file = new File(path);
+            try {
+                file.createNewFile();
+                textInfo.append("File " + file.getName() + " created \n");
+            } catch (IOException e) {
+                textInfo.append("File " + file.getName() + " not  created (" + e.getMessage() + ") (ERROR) \n");
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
